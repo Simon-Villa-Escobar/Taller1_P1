7179982e9618748625eadc5ca99b7cd4157290f1
Simon Villa Escobar <simon.v02@hotmail.com>
2024-02-08 17:42:16 -0500

    Nombre, email y contrasena

--- movie/views.py
+++ movie/views.py
@@ -20,3 +20,7 @@ def home(request):
 def about(request):
     # return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
+
+# Username: simon
+# Email: test@email.com
+# Pw: test
